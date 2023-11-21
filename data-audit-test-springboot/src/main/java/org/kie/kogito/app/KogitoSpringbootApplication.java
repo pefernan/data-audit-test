852d33a2258ba9fbaa7f0172387dfdf56ade354e
Pere Fernàndez <pere.fernandez@gmail.com>
2023-11-21 13:14:51 +0100

    disabling consoles in springboot

--- data-audit-test-springboot/src/main/java/org/kie/kogito/app/KogitoSpringbootApplication.java
+++ data-audit-test-springboot/src/main/java/org/kie/kogito/app/KogitoSpringbootApplication.java
@@ -22,9 +22,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"org.kie.kogito.**"})
-public class KogitoInfinispanSpringbootApplication {
+public class KogitoSpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KogitoInfinispanSpringbootApplication.class, args);
+        SpringApplication.run(KogitoSpringbootApplication.class, args);
     }
 }
